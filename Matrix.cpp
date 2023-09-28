5d697dc0a0154b528d6d89385928fe80f84039f0
venxer <edwinzhao123@gmail.com>
2023-09-28 17:44:32 -0400

    organized functions

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,6 +1,7 @@
 #include "Matrix.h"
 #include <iostream>
 
+//copy constuctor
 Matrix::Matrix(const Matrix &m)
 {
     //copy row and col
@@ -19,6 +20,7 @@ Matrix::Matrix(const Matrix &m)
         }
     }
 }
+//default constuctor
 Matrix::Matrix(unsigned int aRow, unsigned int aCol, double aFill)
 {
     row = aRow;
@@ -37,17 +39,10 @@ Matrix::Matrix(unsigned int aRow, unsigned int aCol, double aFill)
         }
     }
 }
+//destuctor
 Matrix::~Matrix()
 {
     clear();
-}
-int Matrix::num_rows() const
-{
-    return row;
-}
-int Matrix::num_cols() const
-{
-    return col;
 }
 void Matrix::clear()
 {
@@ -61,6 +56,14 @@ void Matrix::clear()
     data = NULL;
     row = 0;
     col = 0;
+}
+int Matrix::num_rows() const
+{
+    return row;
+}
+int Matrix::num_cols() const
+{
+    return col;
 }
 bool Matrix::get(int aRow, int aCol, double &num) const
 {
@@ -80,16 +83,32 @@ bool Matrix::set(const int aRow, const int aCol, const double num)
     data[aRow][aCol] = num; 
     return true;
 }
-
-void Matrix::multiply_by_coefficent(double coefficent)
+double* Matrix::get_row(unsigned int numRow) const
 {
-    for(unsigned int x = 0; x < row; x++)
+    if(row > numRow)
     {
-        for(unsigned int y = 0; y < col; y++)
+        double* output = new double[col];
+        for(unsigned int x = 0; x < col; x++)
         {
-            data[x][y] *= coefficent;
+            output[x] = data[numRow][x];
         }
+        return output;
     }
+    return NULL;
+
+}
+double* Matrix::get_col(unsigned int numCol) const
+{
+    if(col > numCol)
+    {
+        double *output = new double(row);
+        for(unsigned int x = 0; x < row; x++)
+        {
+            output[x] = data[x][col];
+        }
+        return output;
+    }
+    return NULL;
 }
 bool Matrix::swap_row(unsigned int sourceRow, unsigned int targetRow)
 {
@@ -103,6 +122,16 @@ bool Matrix::swap_row(unsigned int sourceRow, unsigned int targetRow)
     data[sourceRow] = data[targetRow];
     data[targetRow] = temp;
     return true;
+}
+void Matrix::multiply_by_coefficent(double coefficent)
+{
+    for(unsigned int x = 0; x < row; x++)
+    {
+        for(unsigned int y = 0; y < col; y++)
+        {
+            data[x][y] *= coefficent;
+        }
+    }
 }
 bool Matrix::add(Matrix &m2)
 {
@@ -137,98 +166,6 @@ bool Matrix::subtract(Matrix &m2)
         return true;
     }
     return false;
-}
-void Matrix::operator=(const Matrix &m1)
-{
-    clear();
-    row = m1.num_rows();
-    col = m1.num_cols();
-    
-    data = new double*[row];
-    for(unsigned int x = 0; x < row; x++)
-    {
-        //create row
-        data[x] = new double[col];
-        for(unsigned int y = 0; y < col; y++)
-        {
-            //allocate data[row][col]
-            double num;
-            m1.get(x, y, num);
-            data[x][y] = num;
-        }
-    }
-}
-bool operator==(const Matrix &m1, const Matrix &m2)
-{
-    //check if same size
-    if(m1.num_rows() == m2.num_rows() && m1.num_cols() == m2.num_cols())
-    {
-        for(int x = 0; x < m1.num_rows(); x++)
-        {
-            for(int y = 0; y < m1.num_cols(); y++)
-            {
-                double value1;
-                double value2;
-                m1.get(x, y, value1);
-                m2.get(x, y, value2);
-                //checks if m1 value = m2 value
-                if(value1 != value2)
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-    return false;
-}
-bool operator!=(const Matrix &m1, const Matrix &m2)
-{
-    return !(m1 == m2);
-}
-std::ostream &operator<<(std::ostream &out_str, const Matrix &m)
-{
-    out_str << "\n" << m.num_rows() << " x " << m.num_cols() <<" matrix:\n" << "[ ";
-    for(int x = 0; x < m.num_rows(); x++)
-    {
-         if(x != 0) out_str << "  ";
-        for(int y = 0; y < m.num_cols(); y++)
-        {
-            double num = 0;
-            m.get(x, y, num);
-            out_str << num << " ";
-        }
-        if(x != m.num_rows() - 1) out_str << "\n";
-    }
-    out_str << "]" << std::endl;
-    return out_str;
-}
-double* Matrix::get_row(unsigned int numRow) const
-{
-    if(row > numRow)
-    {
-        double* output = new double[col];
-        for(unsigned int x = 0; x < col; x++)
-        {
-            output[x] = data[numRow][x];
-        }
-        return output;
-    }
-    return NULL;
-
-}
-double* Matrix::get_col(unsigned int numCol) const
-{
-    if(col > numCol)
-    {
-        double *output = new double(row);
-        for(unsigned int x = 0; x < row; x++)
-        {
-            output[x] = data[x][col];
-        }
-        return output;
-    }
-    return NULL;
 }
 Matrix* Matrix::quarter()
 {
@@ -307,4 +244,69 @@ void Matrix::transpose()
     delete[] data;
     //set data as output which has the transposed data
     data = output;
+}
+void Matrix::operator=(const Matrix &m1)
+{
+    clear();
+    row = m1.num_rows();
+    col = m1.num_cols();
+    
+    data = new double*[row];
+    for(unsigned int x = 0; x < row; x++)
+    {
+        //create row
+        data[x] = new double[col];
+        for(unsigned int y = 0; y < col; y++)
+        {
+            //allocate data[row][col]
+            double num;
+            m1.get(x, y, num);
+            data[x][y] = num;
+        }
+    }
+}
+bool operator==(const Matrix &m1, const Matrix &m2)
+{
+    //check if same size
+    if(m1.num_rows() == m2.num_rows() && m1.num_cols() == m2.num_cols())
+    {
+        for(int x = 0; x < m1.num_rows(); x++)
+        {
+            for(int y = 0; y < m1.num_cols(); y++)
+            {
+                double value1;
+                double value2;
+                m1.get(x, y, value1);
+                m2.get(x, y, value2);
+                //checks if m1 value = m2 value
+                if(value1 != value2)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    return false;
+}
+bool operator!=(const Matrix &m1, const Matrix &m2)
+{
+    return !(m1 == m2);
+}
+std::ostream &operator<<(std::ostream &out_str, const Matrix &m)
+{
+    out_str << "\n" << m.num_rows() << " x " << m.num_cols() <<" matrix:\n" << "[ ";
+    for(int x = 0; x < m.num_rows(); x++)
+    {
+         if(x != 0) out_str << "  ";
+        for(int y = 0; y < m.num_cols(); y++)
+        {
+            double num = 0;
+            m.get(x, y, num);
+            out_str << num << " ";
+        }
+        if(x != m.num_rows() - 1) out_str << "\n";
+    }
+    out_str << "]" << std::endl;
+    return out_str;
 }
