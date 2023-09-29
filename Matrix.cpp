70d93e1643514065fa144abe3cae19bd87177e94
venxer <edwinzhao123@gmail.com>
2023-09-28 22:07:59 -0400

    added comments

--- Matrix.cpp
+++ Matrix.cpp
@@ -53,6 +53,7 @@ void Matrix::clear()
     }
     //delete row
     delete[] data;
+    //reset data, row, col
     data = NULL;
     row = 0;
     col = 0;
@@ -67,7 +68,7 @@ int Matrix::num_cols() const
 }
 bool Matrix::get(int aRow, int aCol, double &num) const
 {
-    //checks neg and not in bound
+    //checks in bound
     if(!((int)row > aRow && (int)col > aCol))
        return false;
 
@@ -76,7 +77,7 @@ bool Matrix::get(int aRow, int aCol, double &num) const
 }
 bool Matrix::set(const int aRow, const int aCol, const double num)
 {
-    //checks neg and not in bound
+    //checks in bound
     if(!((int)row > aRow && (int)col > aCol))
        return false;
     
@@ -85,11 +86,14 @@ bool Matrix::set(const int aRow, const int aCol, const double num)
 }
 double* Matrix::get_row(unsigned int numRow) const
 {
+    //check bound of input
     if(row > numRow)
     {
+        //creates a pointer of doubles length col to store data from row
         double* output = new double[col];
         for(unsigned int x = 0; x < col; x++)
         {
+            //set data of each col from row into output
             output[x] = data[numRow][x];
         }
         return output;
@@ -99,11 +103,14 @@ double* Matrix::get_row(unsigned int numRow) const
 }
 double* Matrix::get_col(unsigned int numCol) const
 {
+    //check bound of input
     if(col > numCol)
     {
+        //creates a pointer of doubles length row to store data from col
         double *output = new double(row);
         for(unsigned int x = 0; x < row; x++)
         {
+            //set data of each row from col into output
             output[x] = data[x][col];
         }
         return output;
@@ -113,7 +120,7 @@ double* Matrix::get_col(unsigned int numCol) const
 bool Matrix::swap_row(unsigned int sourceRow, unsigned int targetRow)
 {
 
-    //checks neg and not in bound
+    //checks in bound
     if(!(row > sourceRow && row > targetRow))
        return false;
     
@@ -125,6 +132,7 @@ bool Matrix::swap_row(unsigned int sourceRow, unsigned int targetRow)
 }
 void Matrix::multiply_by_coefficent(double coefficent)
 {
+    //loops through pointer pointer to multiply each element
     for(unsigned int x = 0; x < row; x++)
     {
         for(unsigned int y = 0; y < col; y++)
@@ -135,8 +143,10 @@ void Matrix::multiply_by_coefficent(double coefficent)
 }
 bool Matrix::add(Matrix &m2)
 {
+    //check bounds of array 
     if(m2.num_rows() == (int)row && m2.num_cols() == (int)col)
     {
+        //loops through pointer pointer to add to each element
         for(unsigned int x = 0; x < row; x++)
         {
             for(unsigned int y = 0; y < col; y++)
@@ -152,8 +162,10 @@ bool Matrix::add(Matrix &m2)
 }
 bool Matrix::subtract(Matrix &m2)
 {
+    //check bounds of array 
     if(m2.num_rows() == (int)row && m2.num_cols() == (int)col)
     {
+        //loops through pointer pointer to subtract from each element
         for(unsigned int x = 0; x < row; x++)
         {
             for(unsigned int y = 0; y < col; y++)
@@ -169,6 +181,7 @@ bool Matrix::subtract(Matrix &m2)
 }
 Matrix* Matrix::quarter()
 {
+    //creates size of each quarter
     int qRow = (int)(0.5 + row/2.0);
     int qCol = (int)(0.5 + col/2.0);
     Matrix* output = new Matrix[4];
@@ -178,6 +191,7 @@ Matrix* Matrix::quarter()
         output[x] = Matrix(qRow, qCol, 0.0);
         int rStart = 0;
         int cStart = 0;
+        //sets the start location of row and col for each iteration
         if(x == 0)
         {
             rStart = 0;
@@ -247,6 +261,7 @@ void Matrix::transpose()
 }
 void Matrix::operator=(const Matrix &m1)
 {
+    //clears and sets size
     clear();
     row = m1.num_rows();
     col = m1.num_cols();
@@ -268,6 +283,7 @@ bool operator==(const Matrix &m1, const Matrix &m2)
     //check if same size
     if(m1.num_rows() == m2.num_rows() && m1.num_cols() == m2.num_cols())
     {
+        //loops though each element to check for equavilency
         for(int x = 0; x < m1.num_rows(); x++)
         {
             for(int y = 0; y < m1.num_cols(); y++)
@@ -293,10 +309,11 @@ bool operator!=(const Matrix &m1, const Matrix &m2)
 }
 std::ostream &operator<<(std::ostream &out_str, const Matrix &m)
 {
+    //loop through matrix and prints in format
     out_str << "\n" << m.num_rows() << " x " << m.num_cols() <<" matrix:\n" << "[ ";
     for(int x = 0; x < m.num_rows(); x++)
     {
-         if(x != 0) out_str << "  ";
+        if(x != 0) out_str << "  ";
         for(int y = 0; y < m.num_cols(); y++)
         {
             double num = 0;
